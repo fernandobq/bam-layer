650b3febf9212cf0ffc9c8edc91969e5a67c5d5f
Fernando Barraza <fernandobarraza@192.168.1.9>
2023-02-07 12:20:10 -0700

    Added the new components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,10 @@
+import { createResolver } from "@nuxt/kit";
+
+const { resolve } = createResolver(import.meta.url)
+
 export default defineNuxtConfig({
     srcDir: 'src',
+    css: [
+        resolve('./assets/styles/main.scss')
+    ]
 })
---
+++ src/components/GlideComponent.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import Glide from '@glidejs/glide'
+
+onMounted(() => {
+    new Glide('.glide').mount()
+})
+
+</script>
+<template>
+    <div class="glide container">
+        <div class="glide__track" data-glide-el="track">
+            <ul class="glide__slides">
+                <li class="glide__slide">0</li>
+                <li class="glide__slide">1</li>
+                <li class="glide__slide">2</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped >
+
+</style>
