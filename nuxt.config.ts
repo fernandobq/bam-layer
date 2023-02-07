9c3f28a789bb178051fc8ce712e0ea236db92fd4
Fernando Barraza <fernandobarraza@192.168.1.9>
2023-02-07 13:16:45 -0700

    Added the style configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,10 @@
+import { createResolver } from "@nuxt/kit";
+
+const { resolve } = createResolver(import.meta.url)
+
 export default defineNuxtConfig({
     srcDir: 'src',
+    css: [
+        resolve('./src/assets/styles/main.scss')
+    ]
 })
