a78ea6cfcf5006dc7ad4b72d14494f33d56bb866
Fernando Barraza <fernandobarraza@192.168.1.9>
2023-02-07 12:26:06 -0700

    changed the nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,3 @@
-import { createResolver } from "@nuxt/kit";
-
-const { resolve } = createResolver(import.meta.url)
-
 export default defineNuxtConfig({
     srcDir: 'src',
-    css: [
-        resolve('./assets/styles/main.scss')
-    ]
 })
